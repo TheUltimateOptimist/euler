b58903dd8871529efd824a6922d3c90e82cb85e4
TheUltimateOptimist <digitalConfidence@web.de>
2023-12-25 15:42:05 +0100

    solved_6

---
+++ 6-sum-square-difference/main.py
@@ -0,0 +1,2 @@
+result = sum([i for i in range(1, 101)])**2 - sum([i**2 for i in range(1, 101)])
+print(f"The difference between the sum of the squares of the first one hundred natural numbers and the square of the sum is:\n{result}")
