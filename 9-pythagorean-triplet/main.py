3fe1559804be0196a48a6949fa758e4b88ada111
TheUltimateOptimist <digitalConfidence@web.de>
2023-12-26 15:27:42 +0100

    solved_9

--- 9-pythagorean-triplet/main.py
+++ 9-pythagorean-triplet/main.py
@@ -17,26 +17,16 @@ def optimized_brute_force() -> tuple[int, int, int]:
             if i*i + j*j == c*c:
                 return (i, j, c)
 
-# copied solution
 @timing
-def another_optimized() -> tuple[int, int, int]:
-    a = 3
-    while True :
-        if a % 2 == 0:
-            b = ((a ** 2) / 4) - 1
-            c = ((a ** 2) / 4) + 1
-        else : 
-            b = ((a ** 2) - 1) // 2
-            c = ((a ** 2) + 1) // 2
-        if 1000 % (a + b + c) == 0 :
-            new_a = a * 1000 / (a + b + c)
-            new_b = b * 1000 / (a + b + c)
-            new_c = c * 1000 / (a + b + c)
-            break
-        else :
-            a += 1
-    return (new_a, new_b, new_c)
+def max_optimized() -> tuple[int, int, int]:
+    a = 32 # a*a >= 1000-a => a >= 32
+    while True:
+        # print(f"{(500_000-1000*a)%(1000-a) + (500_000+a*a-1000*a)%(1000-a)}")
+        if a*a % (1000-a) == 0:
+            b = (500_000-1000*a)//(1000-a)
+            c = (500_000+a*a-1000*a)//(1000-a)
+            return (a, b, c)
+        else: a += 1
 
-
-triplet = another_optimized()
+triplet = max_optimized()
 print(f"The product of the pythagorean triplet {triplet} is:\n{triplet[0]*triplet[1]*triplet[2]}")
